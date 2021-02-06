e953ae68b4c48867febf400523f498ccd99f9467
ecSanders <74225933+ecSanders@users.noreply.github.com>
2021-02-06 01:10:43 -0700

    Update terminal_tweaking.py

--- terminal_tweaking.py
+++ terminal_tweaking.py
@@ -8,3 +8,10 @@ print("'cd' ran with exit code %d" % copy)
 # Meant to fail to show error vs success
 unknown_dir = os.system("cd doesnotexist")
 print("`cd doesnotexis` ran with exit code %d" % unknown_dir)
+
+from Tkinter import Tk     # from tkinter import Tk for Python 3.x
+from tkinter.filedialog import askopenfilename
+
+Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
+filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
+print(filename)
