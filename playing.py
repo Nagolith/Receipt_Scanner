fbbaccaddf4e04d0fea20f138417ecc20943faf3
ecSanders <74225933+ecSanders@users.noreply.github.com>
2021-02-08 16:34:56 -0700

    Update playing.py

--- playing.py
+++ playing.py
@@ -15,20 +15,29 @@ label1.config(font=('helvetica', 20))
 canvas1.create_window(150, 60, window=label1)
 
 def getCSV ():
-    global read_file
+    global image_path
+    global image_name
+    global path
+
+    image_path = filedialog.askopenfilename()
+    image_name = os.path.basename(image_path)
     
-    import_file_path = filedialog.askopenfilename()
-    read_file = pd.read_csv (import_file_path)
+    test = image_path.split(image_name)
+    path = test[0]
     
 browseButton_CSV = tk.Button(text="      Import CSV File     ", command=getCSV, fg='green', bg='red', font=('helvetica', 12, 'bold'))
 canvas1.create_window(150, 130, window=browseButton_CSV)
 
 
 def convertToExcel ():
-    global read_file
-    
-    export_file_path = filedialog.asksaveasfilename(defaultextension='.xlsx')
-    read_file.to_excel (export_file_path, index = None, header=True)
+     new_file_name = 'poppy'
+    clear_dir = 'cd\ '
+    run_tesseract = 'tesseract ' + image_name + ' ' + new_file_name
+
+    os.system(clear_dir)
+    os.system('cd ' + path)
+    os.chdir(path)
+    os.system(run_tesseract)
 
 saveAsButton_Excel = tk.Button(text='Convert CSV to Excel', command=convertToExcel, bg='green', fg='white', font=('helvetica', 12, 'bold'))
 canvas1.create_window(150, 180, window=saveAsButton_Excel)
@@ -51,22 +60,7 @@ root.mainloop()
 
 
     def1:
-    global image_path
-    global image_name
-    global path
-
-    image_path = filedialog.askopenfilename()
-    image_name = os.path.basename(image_path)
     
-    test = image_path.split(image_name)
-    path = test[0]
   
     def2:
-    new_file_name = 'poppy'
-    clear_dir = 'cd\ '
-    run_tesseract = 'tesseract ' + image_name + ' ' + new_file_name
-
-    os.system(clear_dir)
-    os.system('cd ' + path)
-    os.chdir(path)
-    os.system(run_tesseract)
+   
